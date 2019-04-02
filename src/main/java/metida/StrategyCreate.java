329364c3652d57b299ec223b6803da094b95c548
Stelil <deman666xxx@mail.ru>
2019-04-02 16:05:05 +0300

    folder search

--- src/main/java/metida/StrategyCreate.java
+++ src/main/java/metida/StrategyCreate.java
@@ -4,6 +4,9 @@ import metida.interfacable.IUserStrategy;
 import org.joor.Reflect;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 @Component
 public class StrategyCreate {
 
@@ -16,10 +19,42 @@ public class StrategyCreate {
                         "import metida.providers.TankFactoryProvider; \n" +
                         "class CompileTest implements metida.interfacable.IUserStrategy {" +
                         "   public void init() {" +
-                        "       TankFactoryProvider.getTankFactory().getTank();" +
                         "  }\n" +
                         "}\n").create().get();
 
         userStrategy.init();
+
+    }
+
+    static public void main(String arg[]) {
+        File folder = new File("C://");
+        try {
+            processFilesFromFolder(folder);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public void processFilesFromFolder(File folder) {
+        File[] folderEntries = folder.listFiles();
+        for (File entry : folderEntries) {
+            if (entry.isDirectory()) {
+                processFilesFromFolder(entry);
+                continue;
+            } else {
+                final String[] mask = {".class"};
+                String[] files = folder.list(new FilenameFilter() {
+                    @Override
+                    public boolean accept(File folder, String name) {
+                        for (String s : mask)
+                            if (name.toLowerCase().endsWith(s)) return true;
+                        return false;
+                    }
+                });
+
+                for (String fileName : files)
+                    System.out.println("File: " + fileName);
+            }
+        }
     }
 }
